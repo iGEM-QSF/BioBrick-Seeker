eef6e9b00ebb281388b92f7e17148e6ec9f53dac
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-05-30 10:34:48 +0300

    App base

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular
+  .module('iGemPlates2014App', [
+    'ngResource'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('iGemPlates2014App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
