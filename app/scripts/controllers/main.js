a603df622e030264b5e48cd528e7dbd015c23234
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-05-30 15:39:57 +0300

    removed extra stuff from app.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,6 @@
 
 angular.module('iGemPlates2014App')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     $http.get('allkitplates.json').success( function(data, status, headers, config) {
         console.log(data)
