6c042beef77d2a497e25801b6b577015e1cc6257
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-05-30 10:54:40 +0300

    added local json file and printing it on the page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('iGemPlates2014App')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $http.get('allkitplates.json').success( function(data, status, headers, config) {
+        console.log(data)
+        $scope.entries = data;
+    });
   });
