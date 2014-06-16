c8ee79815853f2e644422538da3475c9c3f7616c
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-06-16 14:59:05 +0300

    JSON gets parsed, but is not shown

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,6 @@
 
 angular.module('iGemPlates2014App')
   .controller('MainCtrl', function ($scope, $http) {
-
     $http.get('allkitplates.json').success( function(data, status, headers, config) {
         console.log(data)
         $scope.entries = data;
--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -2,7 +2,6 @@
 
 angular.module('iGemPlates2014App')
   .controller('TeamCtrl', function ($scope, $http) {
-
     $http.get('teams.json').success( function(data, status, headers, config) {
         console.log(data)
         $scope.entries = data;
